42538f0ccd170311cb9e24ed114a207a7dde0eb1
flopshot <n112358@gmail.com>
2017-03-30 09:10:12 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Golf Ranger
+
+###This application is used as a golfing aide in keeping score and gauging distances during a game.
+
+* *In order to debug the google map feature of this app, you must obtain a google maps api key*  
---
+++ mobile/src/main/java/com/sean/golfranger/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.sean.golfranger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Home Screen Activity displayed when app is first launched
+ */
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
