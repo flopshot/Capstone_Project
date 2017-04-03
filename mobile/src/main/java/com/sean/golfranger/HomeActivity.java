6de2db37880fa14162acdecb0a00a72d0ea64b7a
flopshot <n112358@gmail.com>
2017-04-03 14:55:28 -0700

    Feat: Add HomeScreen

--- mobile/src/main/java/com/sean/golfranger/HomeActivity.java
+++ mobile/src/main/java/com/sean/golfranger/HomeActivity.java
@@ -1,15 +1,14 @@
 package com.sean.golfranger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
 
 /**
  * Home Screen Activity displayed when app is first launched
  */
 public class HomeActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,4 +16,27 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
+    public void startNewRound(View v) {
+        Intent intent = new Intent();
+        //intent.setClass(getApplicationContext(), RoundActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewOldRound(View v) {
+        Intent intent = new Intent();
+        //intent.setClass(getApplicationContext(), RoundsActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewPlayers(View v) {
+        Intent intent = new Intent();
+        //intent.setClass(getApplicationContext(), PlayersActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewCourses(View v) {
+        Intent intent = new Intent();
+        //intent.setClass(getApplicationContext(), CoursesActivity.class);
+        startActivity(intent);
+    }
 }
